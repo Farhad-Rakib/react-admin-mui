0150abe98a997aed4987c5ef73277044b54c1752
Farhad Rakib <farhadrakib48@gmail.com>
2023-02-09 11:25:22 +0600

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import {ColorModeContext,useMode} from "./theme"
 import {CssBaseline, ThemeProvider} from "@mui/material"
+import {Routes,Route} from "react-router-dom"
+
+import Sidebar from "./pages/global/Sidebar";
 import Topbar from "./pages/global/Topbar";
+import Dashboard from "./pages/dashboard" ; 
+
+
 
 function App() {
 
@@ -10,8 +16,13 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline/>
         <div className="app">
+          <Sidebar/>
           <main className="content">
             <Topbar/>
+            <Routes>
+                <Route path="/" element={Dashboard}/>
+                
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
